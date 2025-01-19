9507f974e667a176031cbb0b5f140216f5e9b62b
Md Sahil <mdsahilk2003@gmail.com>
2025-01-19 22:07:04 +0530

    Add files via upload
    That is very simple javaScript Program to find the largest number.

---
+++ largestNum.js
@@ -0,0 +1,16 @@
+let a = 55;
+let b = 18;
+let c = 33;
+if(a > b){
+    if(a > c){
+        console.log(a, "a is largest");
+    }else{
+        console.log(c, " c is largest");
+    }
+}else{
+    if(b > c){
+        console.log(b, " b is largest ");
+    }else{
+        console.log(c, " c is largest");
+    }
+}
