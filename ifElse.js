2f32fe4d18b69d50edc867daa39107bf104f3c9d
Md Sahil <mdsahilk2003@gmail.com>
2025-01-19 23:49:10 +0530

    Add files via upload
    if else

---
+++ ifElse.js
@@ -0,0 +1,11 @@
+let marks = 45;
+if(marks >= 33){
+    console.log("Pass");
+    if(marks >= 80){
+        console.log("Grade O");
+    }else{
+        console.log("Grade A");
+    }
+}else{
+    console.log("better luck next time!!");
+}
