4b9f51c1078adb907150316de8361a2bc71db80d
Md Sahil <mdsahilk2003@gmail.com>
2025-01-19 23:55:21 +0530

    Add files via upload
    Array exist in arr or not

---
+++ array.js
@@ -0,0 +1,7 @@
+let arr = ["hello", 'a', 23, 64, 99, -6];
+let item = 64;
+if(arr.indexOf(item) != -1){
+    console.log("Element exist in array");
+}else{
+    console.log("Element dosn't exist in array");
+}
